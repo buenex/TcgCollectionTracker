4cb0efccb6fab31324f7b5c3778c3e9e301642d3
Gabriel Bueno <gabrielbs98@hotmail.com>
2026-02-22 13:03:50 -0300

    fiz ip

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 const PORT = process.env.API_PORT || 3002;
 
-app.listen(PORT,"0,0,0,0", () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
 

e65f7f280d83bcd62f88b0afba10b91763fcb1e5
Gabriel Bueno <gabrielbs98@hotmail.com>
2026-02-22 00:05:05 -0300

    change listening

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 const PORT = process.env.API_PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0,0,0,0", () => {
   console.log("Server running on port", PORT);
 });
 

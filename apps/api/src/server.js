a2c899fd00da4b8c1d2174a776ce6095353a59aa
Gabriel Bueno <gabrielbs98@hotmail.com>
2026-02-22 12:56:20 -0300

    change port

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.API_PORT || 3001;
+const PORT = process.env.API_PORT || 3002;
 
 app.listen(PORT,"0,0,0,0", () => {
   console.log("Server running on port", PORT);
